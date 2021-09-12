9f99986a1340d5ac3be4a83db755719141b230a5
poeynus <poeynus@gmail.com>
2021-09-12 11:45:09 +0900

    알고리즘 9일차

---
+++ Day-9/programmers-네트워크.py
@@ -0,0 +1,22 @@
+def solution(n, computers):
+    answer = 0
+    visited = [False for _ in range(n)]
+
+    for i in range(n):
+        if visited[i] == False:
+                dfs(n, computers, i, visited)
+                answer += 1 
+    return answer
+
+
+def dfs(n, computers, i, visited):
+    visited[i] = True
+    for connect in range(n):
+        if connect != i and computers[i][connect] == 1: 
+            if visited[connect] == False:
+                dfs(n, computers, connect, visited)
+
+
+solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]])
+
+# dfs bfs 플루이드 워셜 초급 문제부터 봐야겠다.
