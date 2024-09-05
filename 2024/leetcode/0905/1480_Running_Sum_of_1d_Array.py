91257ee4faaa9aa4226e72bd534c10fa79fbd27e
poeynus <poeynus@gmail.com>
2024-09-05 10:21:28 +0900

    leet code 1480

---
+++ 2024/leetcode/0905/1480_Running_Sum_of_1d_Array.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        sums = []
+        for i in range(len(nums)):
+            new_num = nums[i] + (sums[i - 1] if i != 0 else 0)
+            sums.append(new_num)
+        return sums
+
+
+class OtherSolution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+        return nums
