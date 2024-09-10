2801c69827ca49dca15f524ff0798baaf480f341
poeynus <poeynus@gmail.com>
2024-09-10 11:25:19 +0900

    leet code 1

---
+++ 2024/leetcode/0910/1_Two_Sum.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+class OtherSolution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
+
+
+OtherSolution().twoSum([-3, 4, 3, 90], 0)
